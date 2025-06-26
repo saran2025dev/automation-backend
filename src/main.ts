ff019c69fd2542db3db97b3cba6c59087e976a16
MD THAHER <thaher@ideassion.com>
2025-06-26 16:42:57 +0530

    .

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,10 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1/documentation', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`http://localhost:${process.env.PORT}`);
+  console.log(`http://localhost:${process.env.PORT}/api/v1/documentation`);
 }
 bootstrap();
